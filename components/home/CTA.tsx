6872bccdc448d1a83c033e4ea3371620018d147b
lausai <lausai360@gmail.com>
2025-10-29 21:50:50 +0800

    修改 cta

--- components/home/CTA.tsx
+++ components/home/CTA.tsx
@@ -8,18 +8,7 @@ const CTA = ({ locale, CTALocale }: { locale: any; CTALocale: any }) => {
       <div className="flex flex-col text-center gap-4">
         <h2 className="text-center">{locale.title}</h2>
         <p className="text-large text-default-500">
-          <RoughNotation type="box" color="#b71c1c" show={true}>
-            {locale.description1}
-          </RoughNotation>{" "}
-          {locale.description2}{" "}
-          <RoughNotation type="box" color="#b71c1c" show={true}>
-            {locale.description3}
-          </RoughNotation>{" "}
-          {locale.description4}{" "}
-          <RoughNotation type="box" color="#b71c1c" show={true}>
-            {locale.description5}
-          </RoughNotation>
-          {locale.description6}
+          {locale.description1}
         </p>
       </div>
       <CTAButton locale={CTALocale} />
